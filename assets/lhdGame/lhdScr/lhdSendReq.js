d837bfd0d703204b80fa5b1d0f2aed023188413c
fantacycao <542033578@qq.com>
2025-03-04 16:46:38 +0800

    龙虎斗游戏 登录消息添加isfree字段

--- assets/lhdGame/lhdScr/lhdSendReq.js
+++ assets/lhdGame/lhdScr/lhdSendReq.js
@@ -9,7 +9,8 @@ cc.Class({
         GameServerManager.send("gameservice.login", "LoginReq", {
             userid: GlobalCfg.USER_DATAS.userId,	    //用户ID
             token: GlobalCfg.USER_DATAS.token,	    //登录服拿到的token
-            fromid: GlobalCfg.PRODUCT_ID       //平台
+            fromid: GlobalCfg.PRODUCT_ID,       //平台
+            // isFree: true,       //是否进入免费场
         });
     },
 
--- library/imports/6a/6abf487a-dc40-4669-8e58-c6b8d1f87898.js
+++ library/imports/6a/6abf487a-dc40-4669-8e58-c6b8d1f87898.js
@@ -15,6 +15,7 @@ cc.Class({
       token: GlobalCfg.USER_DATAS.token,
       //登录服拿到的token
       fromid: GlobalCfg.PRODUCT_ID //平台
+      // isFree: true,       //是否进入免费场
     });
   },
 
