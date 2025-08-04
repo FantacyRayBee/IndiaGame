c6729b3cbfdb2f892b37a9766234d774b325d35c
fantacycao <542033578@qq.com>
2025-08-04 10:39:56 +0800

    1.修改服务器地址 2.关闭自动下载游戏，下载好后直接进入第一个点击的游戏

--- assets/Main/Global.js
+++ assets/Main/Global.js
@@ -1521,10 +1521,10 @@ if (packageChannel && packageChannel.indexOf("_") != -1) {
       break;
     case "5":     // 5服
       GlobalCfg.APP_VERSION = "5.0.0.32"; 
-      GlobalCfg.APP_INFO_URL = `https://download.tkptat.in/production/AppInfo.json`;
-      GlobalCfg.APP_CONFIG_URL = `https://download.tkptat.in/production/AppConfig.json`;
+      GlobalCfg.APP_INFO_URL = `https://svgdownload.tp-game.com/production/AppInfo.json`;
+      GlobalCfg.APP_CONFIG_URL = `https://svgdownload.tp-game.com/production/AppConfig.json`;
       if (cc.sys.localStorage.getItem("UpdateVersion") == "2.4.13") {//cocos 版本2.4.13
-        GlobalCfg.APP_INFO_URL = `https://download.tkptat.in/production/v1/AppInfo.json`;
+        GlobalCfg.APP_INFO_URL = `https://svgdownload.tp-game.com/production/v1/AppInfo.json`;
       }
       // GlobalCfg.APP_INFO_URL = `https://server.tpmass.com/AppInfo.json?time=${new Date().getTime()}`;
       // GlobalCfg.APP_CONFIG_URL = `https://server.tpmass.com/AppConfig.json?time=${new Date().getTime()}`;
--- assets/ResourcesBundle/Scene/Lobby.js
+++ assets/ResourcesBundle/Scene/Lobby.js
@@ -251,6 +251,7 @@ cc.Class({
         this.customMsgEventHandle = ClientNotify.register(GlobalCfg.MSG_TYPE.clientMsg, this.onEventMsg, this);
         this.msgHandle = ClientNotify.register(GlobalCfg.MSG_TYPE.serverMsg, this.onEventMsg, this);
         GlobalCfg.G_COMPONENTS.Audio && GlobalCfg.G_COMPONENTS.Audio.playLobby();
+        this.LoadCompletedCallback = null;
     },
 
 
@@ -881,36 +882,36 @@ cc.Class({
             }
         };
         //自动下载小游戏
-        if (needUpdataArr.length > 0) {
-            let gameSubPackageNames = {
-                "minirocket": "rocket",
-                "minijhandimunda": "munda",
-                "miniteenpatti": "tpGame",
-                "miniteenpattibaccarat": "baccarat3PattiGame",
-                "miniandar": "andaerGame",
-                "minilonghu": "lhdGame",
-                "minishuiguo": "fruitMachine",
-                "minimaya": "mayaMachine",
-                "miniclown": "jokerMachine",
-                "miniindia": "indiaMachine",
-                "minivampire": "vampireMachine",
-                "minibull": "bullMachine",
-                "minisaima": "horseRaceGame",
-                "minibenzbmw": "Benz",
-                "minirummy": "Rummy",
-                "miniseven": "7up7downGame",
-                "minizoo": "zooGame",
-                "minicricket": "cricketGame",
-                "miniluckyloto": "sscGame",
-                "minizeus": "zeusGame",
-            };
-            for (let i = 0, len = this.gameUpdateDownloadOrder.length; i < len; i++) {
-                let element = this.gameUpdateDownloadOrder[i];
-                if (gameSubPackageNames[element] && needUpdataArr.indexOf(gameSubPackageNames[element]) != -1) {
-                    GameDownloader.getInstance().commonLoadGame(gameSubPackageNames[element]);
-                };
-            };
-        };
+        // if (needUpdataArr.length > 0) {
+        //     let gameSubPackageNames = {
+        //         "minirocket": "rocket",
+        //         "minijhandimunda": "munda",
+        //         "miniteenpatti": "tpGame",
+        //         "miniteenpattibaccarat": "baccarat3PattiGame",
+        //         "miniandar": "andaerGame",
+        //         "minilonghu": "lhdGame",
+        //         "minishuiguo": "fruitMachine",
+        //         "minimaya": "mayaMachine",
+        //         "miniclown": "jokerMachine",
+        //         "miniindia": "indiaMachine",
+        //         "minivampire": "vampireMachine",
+        //         "minibull": "bullMachine",
+        //         "minisaima": "horseRaceGame",
+        //         "minibenzbmw": "Benz",
+        //         "minirummy": "Rummy",
+        //         "miniseven": "7up7downGame",
+        //         "minizoo": "zooGame",
+        //         "minicricket": "cricketGame",
+        //         "miniluckyloto": "sscGame",
+        //         "minizeus": "zeusGame",
+        //     };
+        //     for (let i = 0, len = this.gameUpdateDownloadOrder.length; i < len; i++) {
+        //         let element = this.gameUpdateDownloadOrder[i];
+        //         if (gameSubPackageNames[element] && needUpdataArr.indexOf(gameSubPackageNames[element]) != -1) {
+        //             GameDownloader.getInstance().commonLoadGame(gameSubPackageNames[element]);
+        //         };
+        //     };
+        // };
 
         let languagesType = I18NUtil.getInstance().getLanguageType();
         this.setSmallGameBtnByLanguageType(languagesType);
@@ -1984,6 +1985,9 @@ cc.Class({
             return;
         };
 
+        if (this.LoadCompletedCallback == null) {
+            this.LoadCompletedCallback = callFun;
+        }
         if (CommonFun.getInstance().isNeedUpdata(subpackgeName)) {
             CommonFun.getInstance().showTips("Download the game now!");
             GameDownloader.getInstance().priorLoadGame(subpackgeName);
@@ -2150,6 +2154,11 @@ cc.Class({
             default:
                 break;
         };
+
+        if (this.LoadCompletedCallback) {
+            this.LoadCompletedCallback();
+            this.LoadCompletedCallback = null;
+        }
     },
 
     onDestroy: function() {
