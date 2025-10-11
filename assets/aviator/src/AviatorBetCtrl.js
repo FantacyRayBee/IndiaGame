87a7687801cbf6e414649d6e6ca153f77f0b5ea4
fantacycao <542033578@qq.com>
2025-10-11 10:27:26 +0800

    飞机添加下注判断

--- assets/aviator/src/AviatorBetCtrl.js
+++ assets/aviator/src/AviatorBetCtrl.js
@@ -284,6 +284,12 @@ cc.Class({
     },
 
     dealBetEvent: function () {
+        if (GlobalCfg.USER_DATAS.isNotCharge == true && GlobalCfg.USER_DATAS.gamePattern == 0 && GlobalCfg.USER_DATAS.refuseUnpayHundred["miniaviator"] == true){   //未曾充值
+            CommonFun.getInstance().showMsgBox("This feature is available only for premium players . Add cash now to become a premium player .", "SHOP", () => {
+                CommonFun.getInstance().showSmallAddCash()
+            }, false, null, null, null, null, 0.85);
+            return;
+        };
         if (this.betStatus == 0) {//可下注状态
             if (GlobalCfg.USER_DATAS.isNotCharge == true){   //未曾充值
                 CommonFun.getInstance().showMsgBox("This feature is available only for premium players . Add cash now to become a premium player .", "SHOP", () => {
