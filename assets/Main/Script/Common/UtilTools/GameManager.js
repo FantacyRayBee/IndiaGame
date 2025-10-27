38eafdfd061f452bc712e599a148d8d77da7c157
fantacycao <542033578@qq.com>
2025-10-27 16:24:10 +0800

    打开包网人口，替换包网资源

--- assets/Main/Script/Common/UtilTools/GameManager.js
+++ assets/Main/Script/Common/UtilTools/GameManager.js
@@ -1,52 +1,160 @@
 let GameManager = cc.Class({
     ctor: function() {
-        this.gameConfig = [
+    //     this.gameConfig = [
+    //         // PG类型游戏
+    //         // { type: "PG", gameID: "incan-wonders", isVertical: true },
+    //         // { type: "PG", gameID: "mr-treas-fort", isVertical: true },
+    //         { type: "PG", gameID: "cash-mania", isVertical: true },
+    //         // { type: "PG", gameID: "fortune-tiger", isVertical: true },
+    //         // { type: "PG", gameID: "fortune-rabbit", isVertical: true },
+    //         { type: "PG", gameID: "wings-iguazu", isVertical: true },
+    //         { type: "PG", gameID: "fortune-ox", isVertical: true },
+    //         { type: "PG", gameID: "fortune-mouse", isVertical: true },
+    //         { type: "PG", gameID: "fortune-dragon", isVertical: true },
+    //         { type: "PG", gameID: "pinata-wins", isVertical: true },
+    //         // { type: "PG", gameID: "futebol-fever", isVertical: true },
+    //         { type: "PG", gameID: "dragon-hatch", isVertical: true },
+    //         { type: "PG", gameID: "dragon-hatch2", isVertical: true },
+    //         { type: "PG", gameID: "speed-winner", isVertical: true },
+
+    //         // JL类型游戏
+    //         { type: "JL", gameID: "jili_FaFaFa", isVertical: true },
+    //         { type: "JL", gameID: "jili_FortuneGems", isVertical: true },
+    //         { type: "JL", gameID: "jili_FortuneGems2", isVertical: true },
+    //         { type: "JL", gameID: "jili_FortuneGems3", isVertical: true },
+    //         { type: "JL", gameID: "jili_PartyStar", isVertical: true },
+    //         { type: "JL", gameID: "jili_super-rich", isVertical: true },
+    //         { type: "JL", gameID: "jili_chargebuffalo", isVertical: true },
+    //         { type: "JL", gameID: "jili_3potdragons", isVertical: true },
+    //         { type: "JL", gameID: "jili_3devilfire2", isVertical: true },
+    //         { type: "JL", gameID: "jili_chargebuffalo-ascent", isVertical: true },
+    //         { type: "JL", gameID: "jili_3cointreasures", isVertical: true },
+    //         { type: "JL", gameID: "jili_fortunepig", isVertical: true },
+    //         { type: "JL", gameID: "jili_MasterTiger", isVertical: true },
+    //         { type: "JL", gameID: "jili_goldenjoker", isVertical: true },
+    //         { type: "JL", gameID: "jili_legacyofegypt", isVertical: true },
+    //         { type: "JL", gameID: "jili_3luckypiggy", isVertical: true },
+
+    //         // PP类型游戏
+    //         { type: "PP", gameID: "pp_TigreSortudo", isVertical: true },
+    //         { type: "PP", gameID: "pp_TouroSortudo", isVertical: true },
+    //         { type: "PP", gameID: "pp_RatinhoSortudo", isVertical: true },
+    //         { type: "PP", gameID: "pp_MiningRush", isVertical: true },
+    //         { type: "PP", gameID: "pp_SantasXmasRush", isVertical: true },
+    //         { type: "PP", gameID: "pp_GatesofOlympus1000", isVertical: true },
+    //         { type: "PP", gameID: "pp_Moleionaire", isVertical: true },
+    //         { type: "PP", gameID: "pp_CandyCorner", isVertical: true },
+    //         { type: "PP", gameID: "pp_FangtasticFreespins", isVertical: true },
+    //         { type: "PP", gameID: "pp_WisdomofAthena", isVertical: true },
+    //         { type: "PP", gameID: "pp_MysteryMice", isVertical: true }
+    //     ];
+    // },
+
+    this.gameConfig = [
             // PG类型游戏
-            // { type: "PG", gameID: "incan-wonders", isVertical: true },
-            // { type: "PG", gameID: "mr-treas-fort", isVertical: true },
-            { type: "PG", gameID: "cash-mania", isVertical: true },
-            // { type: "PG", gameID: "fortune-tiger", isVertical: true },
-            // { type: "PG", gameID: "fortune-rabbit", isVertical: true },
-            { type: "PG", gameID: "wings-iguazu", isVertical: true },
-            { type: "PG", gameID: "fortune-ox", isVertical: true },
-            { type: "PG", gameID: "fortune-mouse", isVertical: true },
-            { type: "PG", gameID: "fortune-dragon", isVertical: true },
-            { type: "PG", gameID: "pinata-wins", isVertical: true },
-            // { type: "PG", gameID: "futebol-fever", isVertical: true },
-            { type: "PG", gameID: "dragon-hatch", isVertical: true },
-            { type: "PG", gameID: "dragon-hatch2", isVertical: true },
-            { type: "PG", gameID: "speed-winner", isVertical: true },
+            { type: "PG", gameID: "942", isVertical: true },
+            { type: "PG", gameID: "944", isVertical: true },
+            { type: "PG", gameID: "945", isVertical: true },
+            { type: "PG", gameID: "8", isVertical: true },
+            { type: "PG", gameID: "9", isVertical: true },
+            { type: "PG", gameID: "10", isVertical: true },
+            { type: "PG", gameID: "11", isVertical: true },
+            { type: "PG", gameID: "12", isVertical: true },
+            { type: "PG", gameID: "13", isVertical: true },
+            { type: "PG", gameID: "14", isVertical: true },
+            { type: "PG", gameID: "15", isVertical: true },
+            { type: "PG", gameID: "21", isVertical: true },
+            { type: "PG", gameID: "22", isVertical: true },
+            { type: "PG", gameID: "23", isVertical: true },
+            { type: "PG", gameID: "24", isVertical: true },
+            { type: "PG", gameID: "25", isVertical: true },
+            { type: "PG", gameID: "32", isVertical: true },
+            { type: "PG", gameID: "33", isVertical: true },
+            { type: "PG", gameID: "34", isVertical: true },
+            { type: "PG", gameID: "35", isVertical: true },
+            { type: "PG", gameID: "36", isVertical: true },
+            { type: "PG", gameID: "352", isVertical: true },
+            { type: "PG", gameID: "860", isVertical: true },
+            { type: "PG", gameID: "871", isVertical: true },
+            { type: "PG", gameID: "872", isVertical: true },
 
             // JL类型游戏
-            { type: "JL", gameID: "jili_FaFaFa", isVertical: true },
-            { type: "JL", gameID: "jili_FortuneGems", isVertical: true },
-            { type: "JL", gameID: "jili_FortuneGems2", isVertical: true },
-            { type: "JL", gameID: "jili_FortuneGems3", isVertical: true },
-            { type: "JL", gameID: "jili_PartyStar", isVertical: true },
-            { type: "JL", gameID: "jili_super-rich", isVertical: true },
-            { type: "JL", gameID: "jili_chargebuffalo", isVertical: true },
-            { type: "JL", gameID: "jili_3potdragons", isVertical: true },
-            { type: "JL", gameID: "jili_3devilfire2", isVertical: true },
-            { type: "JL", gameID: "jili_chargebuffalo-ascent", isVertical: true },
-            { type: "JL", gameID: "jili_3cointreasures", isVertical: true },
-            { type: "JL", gameID: "jili_fortunepig", isVertical: true },
-            { type: "JL", gameID: "jili_MasterTiger", isVertical: true },
-            { type: "JL", gameID: "jili_goldenjoker", isVertical: true },
-            { type: "JL", gameID: "jili_legacyofegypt", isVertical: true },
-            { type: "JL", gameID: "jili_3luckypiggy", isVertical: true },
+            { type: "JL", gameID: "136", isVertical: true },
+            { type: "JL", gameID: "137", isVertical: true },
+            { type: "JL", gameID: "138", isVertical: true },
+            { type: "JL", gameID: "139", isVertical: true },
+            { type: "JL", gameID: "140", isVertical: true },
+            { type: "JL", gameID: "141", isVertical: true },
+            { type: "JL", gameID: "142", isVertical: true },
+            { type: "JL", gameID: "143", isVertical: true },
+            { type: "JL", gameID: "144", isVertical: true },
+            { type: "JL", gameID: "145", isVertical: true },
+            { type: "JL", gameID: "146", isVertical: true },
+            { type: "JL", gameID: "147", isVertical: true },
+            { type: "JL", gameID: "148", isVertical: true },
+            { type: "JL", gameID: "149", isVertical: true },
+            { type: "JL", gameID: "150", isVertical: true },
+            { type: "JL", gameID: "151", isVertical: true },
+            { type: "JL", gameID: "152", isVertical: true },
+            { type: "JL", gameID: "153", isVertical: true },
+            { type: "JL", gameID: "154", isVertical: true },
+            { type: "JL", gameID: "155", isVertical: true },
+            { type: "JL", gameID: "156", isVertical: true },
+            { type: "JL", gameID: "190", isVertical: true },
+            { type: "JL", gameID: "191", isVertical: true },
+            { type: "JL", gameID: "192", isVertical: true },
 
             // PP类型游戏
-            { type: "PP", gameID: "pp_TigreSortudo", isVertical: true },
-            { type: "PP", gameID: "pp_TouroSortudo", isVertical: true },
-            { type: "PP", gameID: "pp_RatinhoSortudo", isVertical: true },
-            { type: "PP", gameID: "pp_MiningRush", isVertical: true },
-            { type: "PP", gameID: "pp_SantasXmasRush", isVertical: true },
-            { type: "PP", gameID: "pp_GatesofOlympus1000", isVertical: true },
-            { type: "PP", gameID: "pp_Moleionaire", isVertical: true },
-            { type: "PP", gameID: "pp_CandyCorner", isVertical: true },
-            { type: "PP", gameID: "pp_FangtasticFreespins", isVertical: true },
-            { type: "PP", gameID: "pp_WisdomofAthena", isVertical: true },
-            { type: "PP", gameID: "pp_MysteryMice", isVertical: true }
+            { type: "PP", gameID: "361", isVertical: true },
+            { type: "PP", gameID: "362", isVertical: true },
+            { type: "PP", gameID: "363", isVertical: true },
+            { type: "PP", gameID: "365", isVertical: true },
+            { type: "PP", gameID: "366", isVertical: true },
+            { type: "PP", gameID: "368", isVertical: true },
+            { type: "PP", gameID: "369", isVertical: true },
+            { type: "PP", gameID: "370", isVertical: true },
+            { type: "PP", gameID: "371", isVertical: true },
+            { type: "PP", gameID: "410", isVertical: true },
+            { type: "PP", gameID: "411", isVertical: true },
+            { type: "PP", gameID: "412", isVertical: true },
+            { type: "PP", gameID: "413", isVertical: true },
+            { type: "PP", gameID: "414", isVertical: true },
+            { type: "PP", gameID: "415", isVertical: true },
+            { type: "PP", gameID: "417", isVertical: true },
+            { type: "PP", gameID: "433", isVertical: true },
+            { type: "PP", gameID: "446", isVertical: true },
+            { type: "PP", gameID: "471", isVertical: true },
+            { type: "PP", gameID: "472", isVertical: true },
+            { type: "PP", gameID: "474", isVertical: true },
+            { type: "PP", gameID: "477", isVertical: true },
+            { type: "PP", gameID: "481", isVertical: true },
+            { type: "PP", gameID: "510", isVertical: true },
+            { type: "PP", gameID: "519", isVertical: true },
+            { type: "PP", gameID: "846", isVertical: true },
+            { type: "PP", gameID: "847", isVertical: true },
+            { type: "PP", gameID: "861", isVertical: true },
+            { type: "PP", gameID: "862", isVertical: true },
+            { type: "PP", gameID: "899", isVertical: true },
+            { type: "PP", gameID: "900", isVertical: true },
+            { type: "PP", gameID: "901", isVertical: true },
+            { type: "PP", gameID: "999", isVertical: true },
+
+            // quente
+            { type: "quente", gameID: "943", isVertical: true },
+            { type: "quente", gameID: "944", isVertical: true },
+            { type: "quente", gameID: "945", isVertical: true },
+            { type: "quente", gameID: "998", isVertical: true },
+            { type: "quente", gameID: "900", isVertical: true },
+            { type: "quente", gameID: "446", isVertical: true },
+            { type: "quente", gameID: "861", isVertical: true },
+            { type: "quente", gameID: "10", isVertical: true },
+            { type: "quente", gameID: "13", isVertical: true },
+            { type: "quente", gameID: "14", isVertical: true },
+            { type: "quente", gameID: "8", isVertical: true },
+            { type: "quente", gameID: "190", isVertical: true },
+            { type: "quente", gameID: "140", isVertical: true },
+            { type: "quente", gameID: "141", isVertical: true },
+            { type: "quente", gameID: "143", isVertical: true },
         ];
     },
 
@@ -59,17 +167,17 @@ let GameManager = cc.Class({
             }
             tmpCfg[type].push(item); // 把当前项加入对应 type 的数组
         }
-        // 2. 创建 'quente' 类型，包含每个原始类型的前4个游戏
-        tmpCfg['quente'] = [];
-        // 遍历所有原始类型（PG、JL、PP）
-        for (const type in tmpCfg) {
-            // 跳过刚添加的 'quente' 类型本身
-            if (type === 'quente') continue;
-            // 取当前类型的前4个游戏（如果不足4个则全部取）
-            const hotGames = tmpCfg[type].slice(0, 4);
-            // 添加到 'quente' 数组
-            tmpCfg['quente'].push(...hotGames);
-        }
+        // // 2. 创建 'quente' 类型，包含每个原始类型的前4个游戏
+        // tmpCfg['quente'] = [];
+        // // 遍历所有原始类型（PG、JL、PP）
+        // for (const type in tmpCfg) {
+        //     // 跳过刚添加的 'quente' 类型本身
+        //     if (type === 'quente') continue;
+        //     // 取当前类型的前4个游戏（如果不足4个则全部取）
+        //     const hotGames = tmpCfg[type].slice(0, 4);
+        //     // 添加到 'quente' 数组
+        //     tmpCfg['quente'].push(...hotGames);
+        // }
         return tmpCfg;
     },
 
---
+++ assets/ResourcesBundle/huanPi2/lobbyRes/gameIcon/gameIconListCtrl.js
@@ -0,0 +1,162 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        content: cc.Node,
+        game_content: [cc.Node],
+        gamePrefab: cc.Node,
+        toggleParent: cc.Node,
+        toggles: [cc.Toggle],
+
+        btn_close: cc.Button,
+        btn_add: cc.Button,
+
+        lb_coin: cc.Label,
+    },
+    ctor: function () {
+        this.heightList = [];
+    },
+
+    onLoad: function () {
+        this.customMsgEventHandle = ClientNotify.register(GlobalCfg.MSG_TYPE.clientMsg, this.onEventMsg, this);
+
+        for (var i = 0; i < this.toggles.length; i++) {
+            this.toggles[i].node.on('toggle', this.toggleClick, this);
+        }
+
+        this.btn_close.node.on('click', CommonFun.getInstance().debounce(this.btnClick, 1), this);
+        this.btn_add.node.on('click', CommonFun.getInstance().debounce(this.btnClick, 1), this);
+
+        this.gameConfig = GameManager.getInstance().getGameConfig();
+        this.lb_coin.string = CommonFun.getInstance().numberToShow(GlobalCfg.USER_DATAS.userDiamond / 100);
+        this.getHeightList()
+        this.loadItem();
+    },
+    
+    start: function() {
+        this.NowToggleName = "toggle_1"
+        this.setViewByToggleName(this.NowToggleName)
+    },
+
+    onEventMsg: function(webData, target) {
+        let msgId = webData.msgCode;
+        if (msgId == GlobalCfg.CLIENT_MSG_ID.CLOSE_SSCGAME_REFRESH_LOBBY) {
+            this.lb_coin.string = CommonFun.getInstance().numberToShow(GlobalCfg.USER_DATAS.userDiamond / 100);
+        }
+    },
+
+    onDestroy: function () {
+        ClientNotify.removeByHandle(GlobalCfg.MSG_TYPE.clientMsg, this.customMsgEventHandle);
+        CommonFun.getInstance().releasePrefab(GlobalCfg.PREFAB_PATH.GAMEICONLIST);
+    },
+
+    loadItem: function () {
+        this.game_content[0].removeAllChildren();
+        this.game_content[1].removeAllChildren();
+        this.game_content[2].removeAllChildren();
+        this.game_content[3].removeAllChildren();
+        for (var i = 0; i < this.gameConfig['quente'].length; i++) {
+            let data = this.gameConfig['quente'][i];
+            let pab_player = cc.instantiate(this.gamePrefab);
+            pab_player.active = true;
+            pab_player.setPosition(0, 0);
+            let ctrl = pab_player.getComponent('gameIconCtrl');
+            ctrl.setItemData(data.gameID, data.isVertical)
+            this.game_content[0].addChild(pab_player);
+        }
+        for (var i = 0; i < this.gameConfig['JL'].length; i++) {
+            let data = this.gameConfig['JL'][i];
+            let pab_player = cc.instantiate(this.gamePrefab);
+            pab_player.active = true;
+            pab_player.setPosition(0, 0);
+            let ctrl = pab_player.getComponent('gameIconCtrl');
+            ctrl.setItemData(data.gameID, data.isVertical)
+            this.game_content[3].addChild(pab_player);
+        }
+        for (var i = 0; i < this.gameConfig['PP'].length; i++) {
+            let data = this.gameConfig['PP'][i];
+            let pab_player = cc.instantiate(this.gamePrefab);
+            pab_player.active = true;
+            pab_player.setPosition(0, 0);
+            let ctrl = pab_player.getComponent('gameIconCtrl');
+            ctrl.setItemData(data.gameID, data.isVertical)
+            this.game_content[2].addChild(pab_player);
+        }
+        for (var i = 0; i < this.gameConfig['PG'].length; i++) {
+            let data = this.gameConfig['PG'][i];
+            let pab_player = cc.instantiate(this.gamePrefab);
+            pab_player.active = true;
+            pab_player.setPosition(0, 0);
+            let ctrl = pab_player.getComponent('gameIconCtrl');
+            ctrl.setItemData(data.gameID, data.isVertical)
+            this.game_content[1].addChild(pab_player);
+        }
+    },
+
+    getHeightList:function(){
+        let spacing = 5; //间距
+        this.heightList[0] = 0;
+        this.heightList[1] = spacing + this.heightList[0] + this.getHeightByItemNum(this.gameConfig['quente'].length);
+        this.heightList[2] = spacing + this.heightList[1] + this.getHeightByItemNum(this.gameConfig['JL'].length);
+        this.heightList[3] = spacing + this.heightList[2] + this.getHeightByItemNum(this.gameConfig['PP'].length);
+
+        LoggerUtil.getInstance().log('caojun heightList = ', this.heightList);
+    },
+
+    // 根据item的数量计算高度
+    getHeightByItemNum: function(itemNum) {
+        let col = Math.ceil(itemNum / 3); // 得出行数
+        let spacing = 20 * (col - 1); // 行间距
+        let itemHeight = 293;
+        let top = 10;
+        let bottom = 10;
+        let titleImgHeight = 75;
+        let height = titleImgHeight + top + bottom + (col * itemHeight) + spacing;  // 每行3个，向上取整
+        return height;
+    },
+
+    btnClick: function(btn) {
+        let btnName = btn.node.name;
+        if (btnName === "btn_close") {
+            GlobalCfg.G_COMPONENTS.Audio.playBack();
+            this.node.destroy();
+            CommonFun.getInstance().decVerticalAcc();
+            ClientNotify.send(GlobalCfg.MSG_TYPE.clientMsg, {
+                msgCode: "CLOSE_GAMEICONLIST",
+                msgData: {}
+            });
+            return;
+        } 
+        if (btnName === "btn_add") {
+            this.dealBtnAddEvent();
+        } 
+        GlobalCfg.G_COMPONENTS.Audio.playButton();
+    },
+
+    toggleClick: function(toggle) {
+        GlobalCfg.G_COMPONENTS.Audio.playButton();
+        let toggleName = toggle.node.name;
+        this.setViewByToggleName(toggleName);
+    },
+
+    dealBtnAddEvent: function() {
+        if (!GlobalCfg.USER_DATAS.openModules.includes(4)) {
+            CommonFun.getInstance().showMsgBox("Not yet open", "YES_ON", () => { }, false);
+            return
+        };
+        CommonFun.getInstance().showNewShop(true);
+    },
+
+    setViewByToggleName(toggleName) {
+        if(toggleName == this.NowToggleName)
+            return
+
+        for (let i = 0; i < this.game_content.length; i++) {
+            let _toggleName = "toggle_" + (i + 1)
+            if (_toggleName == toggleName) {
+                this.content.position = cc.v2(0, this.heightList[i]);
+            };
+        }
+        this.NowToggleName = toggleName
+    },
+});
