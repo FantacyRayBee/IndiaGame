5282a50b3f14ab4a64a0f9df17f46b9389413d21
fantacycao <542033578@qq.com>
2026-02-06 14:43:03 +0800

    提交代码

--- assets/Main/Global.js
+++ assets/Main/Global.js
@@ -1210,7 +1210,7 @@ if (GlobalCfg.isOfflineDeve == 1) {
 // GlobalCfg.isOfflineDeve2 = 1;
 if (GlobalCfg.is_force_gotoTest == true && !cc.sys.isNative) {
   GlobalCfg.IsDownloadPackage2 = 0;
-  cc.sys.localStorage.setItem("PackageChannel", "5_8001");
+  cc.sys.localStorage.setItem("PackageChannel", "5_1001");
   // GlobalCfg.APP_INFO_URL = `https://server.tpmass.com/AppInfo.json?time=${new Date().getTime()}`;
   // GlobalCfg.APP_CONFIG_URL = `https://server.tpmass.com/AppConfig.json?time=${new Date().getTime()}`;
 };
@@ -1543,18 +1543,11 @@ if (packageChannel && packageChannel.indexOf("_") != -1) {
       break;
     case "5":     // 5服
       GlobalCfg.APP_VERSION = "5.0.0.1"; 
-      // GlobalCfg.APP_INFO_URL = `https://download.mok77.com/production/AppInfo.json`;
-      GlobalCfg.APP_INFO_URL = `https://download.mok77.com/production/AppInfo.json`;
+      GlobalCfg.APP_INFO_URL = `https://download.cj888vip.com/production/AppInfo.json`;
+      GlobalCfg.APP_CONFIG_URL = `https://download.cj888vip.com/production/AppConfig.json`;
 
-      GlobalCfg.APP_CONFIG_URL = `https://download.mok77.com/production/AppConfig.json`;
-      if (cc.sys.localStorage.getItem("UpdateVersion") == "2.4.13") {//cocos 版本2.4.13
-        GlobalCfg.APP_INFO_URL = `https://download.mok77.com/production/v1/AppInfo.json`;
-      }
-      // GlobalCfg.APP_INFO_URL = `https://server.tpmass.com/AppInfo.json?time=${new Date().getTime()}`;
-      // GlobalCfg.APP_CONFIG_URL = `https://server.tpmass.com/AppConfig.json?time=${new Date().getTime()}`;
-
-      GlobalCfg.APP_INFO_URL_SPARE = `https://download.mok77.com/production/AppInfo.json`;
-      GlobalCfg.APP_CONFIG_URL_SPARE = `https://download.mok77.com/production/AppConfig.json`;
+      GlobalCfg.APP_INFO_URL_SPARE = `https://download.cj888vip.com/production/AppInfo.json`;
+      GlobalCfg.APP_CONFIG_URL_SPARE = `https://download.cj888vip.com/production/AppConfig.json`;
       break;
     case "6":     // 代理服
       GlobalCfg.APP_VERSION = "6.0.1";
